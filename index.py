498f0806a89e40c02fc76a7dcccdf94b81b9121b
James <>
2021-04-26 02:18:23 -0700

    Add sum function

--- index.py
+++ index.py
@@ -6,13 +6,19 @@
 # p1 = MyClass()
 # print(p1.x)
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
     
-    def greet(self):
-        print("Hello. My name is " + self.name)
+#     def greet(self):
+#         print("Hello. My name is " + self.name)
 
-p = Person("John", 21)
-p.greet()
+# p = Person("John", 21)
+# p.greet()
+
+def sum(x, y):
+    return int(x) + int(y)
+
+answer = sum(4, "2")
+print(answer)
