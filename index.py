f10729682c42b36cf45cef2fb71d4b4c32c07d4b
James <>
2021-04-25 22:36:29 -0700

    Add __init__()

--- index.py
+++ index.py
@@ -1,7 +1,17 @@
 # print("Hello World")
 
-class MyClass:
-    x = 5
+# class MyClass:
+#     x = 5
 
-p1 = MyClass()
-print(p1.x)
+# p1 = MyClass()
+# print(p1.x)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p = Person("John", 21)
+
+print(p.name)
+print(p.age)
