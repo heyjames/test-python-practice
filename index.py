7fb321bc75bc0527d3c66a5b0d089e1b914711fb
James <>
2021-04-26 02:51:12 -0700

    Add conditionals

--- index.py
+++ index.py
@@ -17,8 +17,23 @@
 # p = Person("John", 21)
 # p.greet()
 
-def sum(x, y):
-    return int(x) + int(y)
+# def sum(x, y):
+#     return int(x) + int(y)
 
-answer = sum(4, "2")
-print(answer)
+# answer = sum(4, "2")
+# print(answer)
+
+print(1 == "0") # False
+print(True == "True") # False
+print(1 == True) # True
+print(0 == False) # True
+print(-1 == True) # False (Interesting...)
+print(-1 == False) # False (Interesting...)
+print("" == None) # False
+print(False == None) # False
+print([] == None) # False
+iAmNothing = None
+print(iAmNothing == None) # True
+print("lem" in "element") # True
+print("LEM" in "element") # False
+print("lem" not in "element") # False
