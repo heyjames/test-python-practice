1748860592103a32a0f48bdd559d7937c5b9f7d8
James <>
2021-04-26 02:52:11 -0700

    Add substring conditionals

--- index.py
+++ index.py
@@ -23,17 +23,35 @@
 # answer = sum(4, "2")
 # print(answer)
 
-print(1 == "0") # False
-print(True == "True") # False
-print(1 == True) # True
-print(0 == False) # True
-print(-1 == True) # False (Interesting...)
-print(-1 == False) # False (Interesting...)
-print("" == None) # False
-print(False == None) # False
-print([] == None) # False
-iAmNothing = None
-print(iAmNothing == None) # True
-print("lem" in "element") # True
-print("LEM" in "element") # False
-print("lem" not in "element") # False
+# print(1 == "0") # False
+# print(True == "True") # False
+# print(1 == True) # True
+# print(0 == False) # True
+# print(-1 == True) # False (Interesting...)
+# print(-1 == False) # False (Interesting...)
+# print("" == None) # False
+# print(False == None) # False
+# print([] == None) # False
+# iAmNothing = None
+# print(iAmNothing == None) # True
+# print("lem" in "element") # True
+# print("LEM" in "element") # False
+# print("lem" not in "element") # False
+
+# https://stackoverflow.com/a/3437068
+s = "This be a string"
+if s.find("is") == -1: # False and True doesn't work
+    print("No 'is' here!")
+else:
+    print("Found 'is' in the string.")
+
+s = "This be a string"
+if "is" not in s:
+    print("No 'is' here!")
+else:
+    print("Found 'is' in the string.")
+
+any_string = "This"
+start = 0
+stop = len(any_string)
+print(any_string.find('is', start, stop)) # 2. Returns start index of substring
