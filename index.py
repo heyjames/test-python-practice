e942dc59ade607f7065570d48d65669c47fcc909
James <>
2021-04-26 02:55:20 -0700

    Add exceptions and loops

--- index.py
+++ index.py
@@ -39,19 +39,34 @@
 # print("lem" not in "element") # False
 
 # https://stackoverflow.com/a/3437068
-s = "This be a string"
-if s.find("is") == -1: # False and True doesn't work
-    print("No 'is' here!")
-else:
-    print("Found 'is' in the string.")
+# s = "This be a string"
+# if s.find("is") == -1: # False and True doesn't work
+#     print("No 'is' here!")
+# else:
+#     print("Found 'is' in the string.")
 
-s = "This be a string"
-if "is" not in s:
-    print("No 'is' here!")
-else:
-    print("Found 'is' in the string.")
+# s = "This be a string"
+# if "is" not in s:
+#     print("No 'is' here!")
+# else:
+#     print("Found 'is' in the string.")
 
-any_string = "This"
-start = 0
-stop = len(any_string)
-print(any_string.find('is', start, stop)) # 2. Returns start index of substring
+# any_string = "This"
+# start = 0
+# stop = len(any_string)
+# print(any_string.find('is', start, stop)) # 2. Returns start index of substring
+
+x = -1
+if x < 0:
+    raise Exception("Numbers below zero are not allowed.")
+
+myList = ["Tony Stark", "Steve Rogers"]
+for element in myList:
+    print(element)
+
+for i in range(0, 4):
+    print(i, "I can do this all day.")
+
+metal = "Vibranium"
+for i in range(0, len(metal)):
+    print(metal[i])
